dc3e6ec694179552b691d605fbad43fa9bbea16e
Devendra Kumar <85422818+dkrana5298@users.noreply.github.com>
2022-01-24 17:48:05 +0530

    added final with included leap year

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function age() {
     var d1 = document.getElementById('date1').value;
+    d1=parseInt(d1)
+    if(d1 === ""){
+      alert("You can not leave date field empty: ")
+      return;
+    }
     var m1 = document.getElementById('month1').value;
+    m1=parseInt(m1)
+    if(m1 === ""){
+      alert("You can not leave month field empty: ")
+      return;
+    }
     var y1 = document.getElementById('year1').value;
-  
+    y1=parseInt(y1)
+    if(y1 === ""){
+      alert("You can not leave year field empty: ")
+      return;
+    }
     var d2 = document.getElementById('date2').value;
+    d2=parseInt(d2)
+    if(d2 === ""){
+      alert("You can not leave date field empty: ")
+      return;
+    }
     var m2 = document.getElementById('month2').value;
+    m2=parseInt(m2);
+    if(m2 === ""){
+      alert("You can not leave month field empty: ")
+      return;
+    }
     var y2 = document.getElementById('year2').value;
+    y2=parseInt(y2)
+    if(y2 === ""){
+      alert("You can not leave year field empty: ")
+      return;
+    }
 
     var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   
@@ -17,18 +46,26 @@ function age() {
     if(m1 > m2){
       m2 = m2 + 12;
       y2 = y2 - 1;
-      
     }
     var d = d2 - d1;
-    if(d>100){
-     d= parseInt(d/100);
-    }
+    
     var m = m2 - m1;
-    if(m>100){
-      m=parseInt(m/100);
+    if(y1 > y2){
+      alert("You Can not put year greater then year of below:  ")
+      return;
     }
     var y = y2 - y1;
-   
+    var leap=0;
+    for(var i=y1 + 1; i<=y2; i++){
+      if(( i % 4 == 0) && ( i % 100 != 0) || ( i % 400 == 0)){
+        leap++
+      }
+    }
+    d=d+leap;
+
+    if(d > 30){
+      m = m + 1 ;
+    }
     document.getElementById('age').innerHTML = +y+' Years, '+m+' Months '+d+' Days';
     var final_year=y + m/12 + d/365 ;
     document.getElementById('years').innerHTML="=  "+final_year.toFixed(2)+"  years";
